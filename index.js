0cccc9b6e160d75d521a4c1e2299706228ef99c5
Moayyad Saleh <moayyadalazzam@gmail.com>
2023-07-31 15:18:52 -0600

    diploy app

--- index.js
+++ index.js
@@ -12,31 +12,78 @@ app.use(express.static("public")); // Serve static files from the "public" direc
 app.use(bodyParser.urlencoded({ extended: true })); // Parse incoming URL-encoded data
 app.set("view engine", "ejs"); // Set the view engine to EJS for rendering templates
 
+// API URL for fetching recipe from TheCocktailDB
+const API_URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
-
-// API URL for fetching jokes from JokeAPI
-const API_URL = "www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
-
-// Handle GET request on the root route ("/")
-app.get("/", async (req, res) => {
+// Update the GET route to return an array of cocktails
+// Update the GET route to return a random cocktail
+// Update the GET route to return a random cocktail
+app.get("/cocktails", async (req, res) => {
   try {
-    // Fetch a joke from the JokeAPI
-    const response = await axios.get(API_URL);
-    const result = response.data;
-    console.log(result); // Log the fetched joke for debugging purposes
+    const cocktailName = req.query.name; // Get the cocktail name from the query parameter
+    if (!cocktailName) {
+      // If no cocktail name is provided, render the "index.ejs" template with an error message
+      return res.render("index.ejs", {
+        error: "Please provide a cocktail name.",
+      });
+    }
 
-    // Render the "index.ejs" template with the fetched joke data
-    res.render("index.ejs", { data: result });
+    // Fetch a list of cocktails from the TheCocktailDB API using the provided cocktailName
+    const response = await axios.get(API_URL + "search.php", {
+      params: {
+        s: cocktailName, // Add the cocktail name as a query parameter
+      },
+    });
+    const cocktailData = response.data.drinks; // Use a descriptive variable name
+
+    if (!cocktailData || cocktailData.length === 0) {
+      // If no cocktails are found, render the "index.ejs" template with an error message
+      return res.render("index.ejs", {
+        error: "Sorry, No cocktail found. Please try again later.",
+      });
+    }
+
+    // Render the "index.ejs" template with the cocktailData
+    return res.render("index.ejs", { cocktails: cocktailData });
   } catch (error) {
-    // If an error occurs during the fetch, render the "index.ejs" template with an error message
-    console.error("Failed to make request:", error.message);
-    res.render("index.ejs", {
-      error: error.message,
+    // Handle errors
+    console.error("Error fetching cocktails:", error);
+    return res.render("index.ejs", {
+      error: "An error occurred while fetching cocktails. Please try again later.",
     });
   }
 });
 
+// Handle POST request on the root route ("/")
+app.post("/", async (req, res) => {
+  try {
+    // Extract the selected category from the request body
+    const searchQuery = req.body.searchQuery;
+
+    // Update the API URL to include the selected category and the API key
+    const API_URL_WITH_QUERY = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchQuery}`;
+
+    // Fetch cocktail data from TheCocktailDB API based on the search query and API key
+    const response = await axios.get(API_URL_WITH_QUERY);
+    const drinks = response.data.drinks; // Access the drink data
+
+    // Render the "index.ejs" template with the newly fetched cocktail data
+    res.render("index.ejs", {
+      data: drinks, // Sending the fetched cocktail data to the template
+      error: null,
+    });
+  } catch (error) {
+    // If an error occurs during the fetch, render the "index.ejs" template with an error message
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: "Failed to fetch cocktail data. Please try again later.",
+      data: null,
+    });
+  }
+});
+
+
 // Start the server and listen on the specified port
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+  console.log(`Server is running on port ${port}`);
+});
